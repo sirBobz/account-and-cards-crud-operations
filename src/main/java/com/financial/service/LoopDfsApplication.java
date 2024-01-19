d95e2e4132a221b52f650c12e443f145aa164121
Bob Mwenda  Murithi <bmmurithi@safaricom.co.ke>
2024-01-19 20:03:17 +0300

    initial commit

---
+++ src/main/java/com/financial/service/LoopDfsApplication.java
@@ -0,0 +1,13 @@
+package com.financial.service;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LoopDfsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LoopDfsApplication.class, args);
+	}
+
+}
